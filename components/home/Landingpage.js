ce944d0fea88a92a2cb4b63ebca766b7c5cdb066
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-06-18 13:59:00 +0530

    added will launch soon

---
+++ components/home/Landingpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landingpage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl md:text-[6rem] font-extrabold text-white">Will Launch Soon</h1>
+        </div>
+    );
+};
+
+export default Landingpage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
