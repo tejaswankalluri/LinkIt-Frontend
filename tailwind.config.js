38a857869af7984668036052be170bdf6315ba02
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-06-18 15:25:21 +0530

    added colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
             fontFamily: {
                 inter: ['Inter', 'sans-serif'],
             },
+            colors: {
+                darkbgvoilet: '#2D2340',
+                lightbggrape: '#4F3E6E',
+                voilet: '#8C52FF',
+                lightpurple: '#B4A1DA',
+                skyblue: '#8BC6EC',
+                ceil: '#9599E2',
+                pictonBlue: '#43C5FF',
+                phlox: '#E520FF',
+                sunsetorange: '#F55555',
+                sunglow: '#FCCF31',
+            },
         },
     },
     plugins: [],
