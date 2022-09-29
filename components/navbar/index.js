9e3e6480ae862aef18c4b2b872d43a8bb6d008b1
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-09-29 19:26:48 +0530

    update navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 function Navbar() {
     return (
         <div className="bg-[#403359]">
             <nav className="container mx-auto max-h-36 flex items-center justify-between text-lightpurple py-4">
                 <div className="text-2xl cursor-pointer">
-                    <Image src="/nav_logo.svg" alt="logo" width={95} height={48} />
+                    <Link href="/">
+                        <Image src="/nav_logo.svg" alt="logo" width={95} height={48} />
+                    </Link>
+
                     {/* <img src="/nav_logo.svg" alt="" /> */}
                 </div>
                 <div className="flex w-80 justify-between items-center cursor-pointer font-semibold">
@@ -18,7 +22,9 @@ function Navbar() {
                     <ul>
                         <a href="">Pricing</a>
                     </ul>
-                    <button className="border-none bg-voilet py-2 px-5 rounded-md text-white">Login</button>
+                    <Link href="/signin">
+                        <button className="border-none bg-voilet py-2 px-5 rounded-md text-white">Login</button>
+                    </Link>
                 </div>
             </nav>
         </div>
